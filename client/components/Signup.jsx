e6c6d976abd09bc15eca42cc731c3372db048913
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-03-06 15:41:11 -0500

    Create signup component

---
+++ client/components/Signup.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form>
+        <input name="username" placeholder="Username" type="text" />
+        <input name="password" placeholder="Password" type="password" />
+        <input type="submit" value="Sign up" />
+      </form>
+    );
+  }
+}
+
+export default Signup;
