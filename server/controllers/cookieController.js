c8bd80c9b03713c688aac70858f341b5b01d5b20
johnli <jli159@binghamton.edu>
2021-03-06 16:08:35 -0500

    UserController and CookieController added 	Created basic routes to handle POST requests to /signup and /login. UserController has createUser and verifyUser functions.
    	createUser checks if username input already exists in database. If so, res.locals.alreadyExists will be saved as true. If not, the new user is added to the database and the _id from the newly added record is saved onto res.locals.id. As of now, the end of the route will send back a json string saying "New user added!" if new user was successfully added or "Username already taken!" if username already existed in database.
    
    	verifyUser checks if username and password matches existing record in database. If so, _id and username from the record is stored onto res.locals.userRecord. If not, res.locals.loggedIn will be set to true. The following middleware will check the flag to see if it should run. The end of the route will send "Incorrect/username/password" if not logged in. John to catch up with team to determine how authenication routing should work with front end and update accordingly.
    
    	Session work started.

---
+++ server/controllers/cookieController.js
@@ -0,0 +1,14 @@
+const db = require('../models/quizModels');
+
+const cookieController = {};
+
+cookieController.setSSIDCookie = (req, res, next) => {
+  if (res.locals.loggedIn) {
+    res.cookie('ssid', res.locals.userRecord._id, { httpOnly: true });
+    return next();
+  } else {
+    return next();
+  }
+};
+
+module.exports = cookieController;
--- server/server.js
+++ server/server.js
@@ -3,27 +3,50 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const userController = require('./controllers/userController');
+const cookieController = require('./controllers/cookieController');
+
 const app = express();
 
-app.use('/build', express.static(path.resolve(__dirname, '../build')))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req,res)=> {
-    //get request to login/ signup page
-    return res.status(200).sendFile(path.resolve(__dirname,'../index.html'))
-})
+app.use('/build', express.static(path.resolve(__dirname, '../build')));
 
-app.get('/quizoverflow', (req,res)=>{
-    //dummy function, placeholder for information to be sent back to client
-    res.status(200)
-    res.send()
-})
+app.get('/', (req, res) => {
+  //get request to login/ signup page
+  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+});
 
-app.use((req, res, next)=>{
-    res.status(404)
-    .send('Not Found')
-}
+app.post('/signup', userController.createUser, (req, res) => {
+  // on failed signup, send boolean false
+  if (res.locals.alreadyExists)
+    return res.status(200).json('Username already taken!');
+  // on successful signup, send boolean true
+  res.status(200).json('New user added!');
+});
 
-)
+app.post(
+  '/login',
+  userController.verifyUser,
+  cookieController.setSSIDCookie,
+  (req, res) => {
+    if (!res.locals.loggedIn)
+      return res.status(200).json('Incorrect username/password');
+    res.status(200).json(res.locals.userRecord);
+  }
+);
+
+app.get('/quizoverflow', (req, res) => {
+  //dummy function, placeholder for information to be sent back to client
+  res.status(200);
+  res.send();
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
+});
 
 app.listen(3000);
-console.log('Server listening on port 3000')
+console.log('Server listening on port 3000');
