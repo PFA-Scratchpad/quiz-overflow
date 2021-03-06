ed8e657b420ed8eb4af41e733b32c8673e27cad8
dwayneRichards <dwaynerichards@gmail.com>
2021-03-05 21:41:14 -0500

    added middleware functions, no DB connection

--- server/server.js
+++ server/server.js
@@ -8,8 +8,22 @@ const app = express();
 app.use('/build', express.static(path.resolve(__dirname, '../build')))
 
 app.get('/', (req,res)=> {
+    //get request to login/ signup page
     return res.status(200).sendFile(path.resolve(__dirname,'../index.html'))
 })
+
+app.get('/quizoverflow', (req,res)=>{
+    //dummy function, placeholder for information to be sent back to client
+    res.status(200)
+    res.send()
+})
+
+app.use((req, res, next)=>{
+    res.status(404)
+    .send('Not Found')
+}
+
+)
 
 app.listen(3000);
 console.log('Server listening on port 3000')
