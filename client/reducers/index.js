ffec994f88c94cbab3b064da8a44a50a52cca5e8
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-03-07 16:15:23 -0500

    Add new reducers to combine

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import quizReducer from './quizReducer';
+import signup from './signupReducer';
+import login from './loginReducer';
 
 const reducers = combineReducers({
+  signup,
+  login,
   quiz: quizReducer,
 });
 
 export default reducers;
+
